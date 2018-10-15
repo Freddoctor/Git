0903c848ff2b9d8d6ab481f8397ab79f7e8b4160
Freddoctor <642582009@qq.com>
2018-10-15 14:53:54 +0800

    handlebars 模板引入

--- fx168-m/src/handlebars.js
+++ fx168-m/src/handlebars.js
@@ -1,16 +1,19 @@
 import "jquery";
 import "./css/home.css"
 var template = require("./asset/handlebars.hbs");
+
+var parms = {
+  images: {
+    index: require("./img/img_h5_v.2.0/shouye1@2x.png"),
+    news: require("./img/zixun.png"),
+    sudi: require("./img/sudi.png"),
+    quote: require("./img/hangqing.png"),
+    calendar: require("./img/cjrl.png"),
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   var div = document.createElement('div');
-  div.innerHTML = template({
-    images: {
-      index: require("./img/img_h5_v.2.0/shouye1@2x.png"),
-      news: require("./img/zixun.png"),
-      sudi: require("./img/sudi.png"),
-      quote: require("./img/hangqing.png"),
-      calendar: require("./img/cjrl.png"),
-    }
-  });
+  div.innerHTML = template(parms);
   document.body.appendChild(div);
 });
