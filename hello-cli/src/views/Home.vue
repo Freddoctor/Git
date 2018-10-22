e2ba5c22ac0a351ee323bd4186e414a0d3acc92a
Freddoctor <642582009@qq.com>
2018-10-22 15:13:35 +0800

    hello vue

--- hello-cli/src/views/Home.vue
+++ hello-cli/src/views/Home.vue
@@ -1,18 +1,28 @@
+<style  lang="scss" scoped>
+
+
+
+</style>
+
 <template>
-  <div class="home">
+
+<div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+</div>
+
 </template>
 
 <script>
+
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    components: {
+        HelloWorld
+    }
 }
+
 </script>
