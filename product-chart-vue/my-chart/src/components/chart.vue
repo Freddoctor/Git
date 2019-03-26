1b7b9f2abb7ccbf6172c339235a57c56bc47709b
Freddoctor <642582009@qq.com>
2019-03-26 13:36:38 +0800

    typescript 新增

--- product-chart-vue/my-chart/src/components/chart.vue
+++ product-chart-vue/my-chart/src/components/chart.vue
@@ -1,11 +1,9 @@
+<style lang="sass">
+
+</style>
+
 <template>
   <div>
-    <input v-model="msg">
-    <p>prop: {{propMessage}}</p>
-    <p>msg: {{msg}}</p>
-    <p>helloMsg: {{helloMsg}}</p>
-    <p>computed msg: {{computedMsg}}</p>
-    <button @click="greet">Greet</button>
   </div>
 </template>
 
@@ -38,4 +36,6 @@ export default class Chart extends Vue {
     alert("greeting: " + this.msg);
   }
 }
+
+
 </script>
